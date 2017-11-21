ba0657e1e710c36060b7f23823c9e3172a6dd959
Josh Klein <jGlass314@gmail.com>
2017-11-21 12:33:06 -0600

    recursive solution v1

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,21 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// document.body, element.childNodes, and element.classList
+
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var root = document.body;
+  var arr = [];
+  return recurse(className, root, arr);
 };
+var recurse = function(className, element, arr) {
+  if(element.length === 0)
+    return arr;
+  if(element.classList !== undefined && element.classList.contains(className)) {
+    arr.push(element);
+  }
+  for(var i = 0; i < element.childNodes.length; i++)
+    recurse(className, element.childNodes[i], arr);
+  return arr;
+}
